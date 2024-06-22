befa4b792e2d494fc54dbc13d177366352f15b33
djangomail <djangomail97@gmail.com>
2024-06-22 23:55:02 +0100

    edited the views , urls and templates

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from datetime import datetime
 # Create your views here.
+def home(request):
+    nowdate = datetime.now()
+    now = nowdate.strftime("%H:%M:%S")
+    return render(request, 'app/index.html', {'now':now})
