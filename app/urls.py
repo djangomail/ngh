5efc9dfd7103b63ca068d9e56ff722fbd66f4b7f
djangomail <djangomail97@gmail.com>
2024-06-24 19:35:16 +0100

    added htmx for live time update

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, update
 
 urlpatterns= [
     path("", home, name="home"),
+    path("update", update, name="update"),
 ]
--- app/views.py
+++ app/views.py
@@ -4,6 +4,9 @@ from .models import Student
 # Create your views here.
 def home(request):
     student = Student.objects.all()
+    return render(request, 'app/index.html', {"student":student})
+    
+def update(request):
     nowdate = datetime.now()
     now = nowdate.strftime("%H:%M:%S")
-    return render(request, 'app/index.html', {'now':now, "student":student})
+    return render(request, "partial/time.html", {'now':now})
