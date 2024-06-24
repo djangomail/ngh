039237d9a699af9efb384e037403a4716c9546f8
djangomail <djangomail97@gmail.com>
2024-06-24 18:49:43 +0100

    created database and added some data to it . installing psycopg2-binary

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from datetime import datetime
+from .models import Student
 # Create your views here.
 def home(request):
+    student = Student.objects.all()
     nowdate = datetime.now()
     now = nowdate.strftime("%H:%M:%S")
-    return render(request, 'app/index.html', {'now':now})
+    return render(request, 'app/index.html', {'now':now, "student":student})
--- new/settings.py
+++ new/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 from decouple import config
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -78,10 +79,9 @@ WSGI_APPLICATION = 'new.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
